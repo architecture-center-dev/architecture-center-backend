46b1c3cffd00192b33394a03e827c67507373ffc
Robisson Oliveira <robisson@robissonoliveira.com.br>
2021-02-13 17:38:04 -0300

    Add more tests

--- src/solutions/domain/services/solutions.service.spec.ts
+++ src/solutions/domain/services/solutions.service.spec.ts
@@ -1,18 +1,76 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import { Solution } from '../entities/solution.entity';
 import { SolutionsService } from './solutions.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { CanvasService } from './canvas.service';
+import { CreateSolutionDto } from '../dto/create-solution.dto';
+import { ObjectID } from 'mongodb';
+import { Canvas } from '../entities/canvas.entity';
 
-describe('SolutionsService', () => {
-  let service: SolutionsService;
+describe('Solution Service', () => {
+  let solutionsService: SolutionsService;
+  let findOne: () => {} = jest.fn();
+  let find: () => {} = jest.fn();
+  let createSolutionDto: CreateSolutionDto = {
+    big_picture: "url_image",
+    customer: "customer",
+    description: "desccription",
+    market: "market",
+    name: "name",
+    project: "project",
+    tags: "tags",
+    team_members: "teams",
+    year_month: "10/2020"
+  }
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [SolutionsService],
-    }).compile();
+    const module = await Test.createTestingModule({
+      providers: [
+        SolutionsService,
+        CanvasService,
+        {
+          provide: getRepositoryToken(Solution),
+          useValue: {
+            save: () => ({ ...createSolutionDto, solution_id: new ObjectID() }),
+            findOne,
+            find
+          }
+        },
+        {
+          provide: getRepositoryToken(Canvas),
+          useValue: {
+            save: () => jest.fn()
+          }
+        }
+      ],
+    })
+      .compile();
 
-    service = module.get<SolutionsService>(SolutionsService);
-  });
+    solutionsService = await module.get(SolutionsService);
+  })
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-  });
+  it('create method should return Solution object', async () => {
+
+
+    let solution: Solution = await solutionsService.create(createSolutionDto);
+
+    expect(solution).toMatchObject(createSolutionDto);
+
+  })
+
+  it('findOne method should call findOne in the SolutionRepository', async () => {
+
+    await solutionsService.findOne({});
+
+    expect(findOne).toBeCalled();
+
+  })
+  
+  it('findAll method should call find in the SolutionRepository', async () => {
+
+    await solutionsService.findAll("");
+
+    expect(find).toBeCalled();
+
+  })
 });
